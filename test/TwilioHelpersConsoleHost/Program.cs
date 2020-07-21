800f9ded9ad43f0cc530c7bc5e3d84bf707d1e22
npnelson <nnelson1@insight.rr.com>
2020-07-21 15:59:58 -0400

    [skip ci] added console test harness

---
+++ test/TwilioHelpersConsoleHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetToolBox.TwilioHelpers.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace TwilioHelpersConsoleHost
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IConfiguration Configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+            var sp = new ServiceCollection();
+            sp.AddTwilioServices(Configuration.GetSection("TwilioSettings"));
+            var services = sp.BuildServiceProvider();
+            var twilio = services.GetRequiredService<ITwilioServices>();
+            await twilio.SendSMSMessageAsync("TestText", "", "");
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
